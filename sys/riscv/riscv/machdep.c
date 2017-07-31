8c306d91446d47bfa769d6a34372463f862d8469
Ruslan Bukin <br@bsdpad.com>
2017-07-31 15:44:14 +0100

    cleanup

--- sys/riscv/riscv/machdep.c
+++ sys/riscv/riscv/machdep.c
@@ -719,7 +719,7 @@ try_load_dtb(caddr_t kmdp, vm_offset_t dtbp)
 {
 
 #if defined(FDT_DTB_STATIC)
-	//dtbp = (vm_offset_t)&fdt_static_dtb;
+	dtbp = (vm_offset_t)&fdt_static_dtb;
 #endif
 
 	if (dtbp == (vm_offset_t)NULL) {
